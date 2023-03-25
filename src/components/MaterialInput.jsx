8af8c8ec3c223c3bb8c8e7cafddb614e0b2cd862
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2023-03-25 14:13:32 -0400

    Add size constraint to MaterialInput

--- src/components/MaterialInput.jsx
+++ src/components/MaterialInput.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-function MaterialInput({ state, setState, label }) {
+function MaterialInput({ state, setState, label, size }) {
 
     const [focused, setFocused] = useState(false);
 
     return (
         // Material input container will be position relative with the input inside it. The ::before makes the border bottom, and the ::after makes the animated border
         <div className={"material-input-container" + (focused ? " active" : "")}>
-            <input className="material-input" onFocus={() => setFocused(true)} onBlur={() => setFocused(false)} placeholder={label} value={state} onChange={({ target: { value } }) => setState(value)} />
+            <input size={size || 16} className="material-input" onFocus={() => setFocused(true)} onBlur={() => setFocused(false)} placeholder={label} value={state} onChange={({ target: { value } }) => setState(value)} />
         </div>
     )
 }
