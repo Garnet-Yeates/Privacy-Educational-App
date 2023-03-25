cecb58053d78e67ecf1e6645ce464dd7e2f3d765
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2023-03-25 01:40:28 -0400

    Set up Phishing page

--- src/Pages/PhishingPage/PhishingPage.jsx
+++ src/Pages/PhishingPage/PhishingPage.jsx
@@ -7,7 +7,18 @@ function PhishingPage() {
 
     return (
         <div className="phishing-page">
+            <div className="opacity-overlay">
+                <div className="card">
+                    <div className="card-header">
 
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-heading">
+                            Enter your Username and Password
+                        </h5>
+                    </div>
+                </div>
+            </div>
         </div >)
 }
 
