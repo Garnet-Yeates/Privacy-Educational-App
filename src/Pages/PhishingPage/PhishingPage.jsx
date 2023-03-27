f5b4e01632ef55f540e434cfe56b868ea1c7fe69
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2023-03-26 23:36:35 -0400

    Phishing page 90% done

--- src/Pages/PhishingPage/PhishingPage.jsx
+++ src/Pages/PhishingPage/PhishingPage.jsx
@@ -3,17 +3,36 @@ import { useMemo, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import MaterialCheckbox from '../../components/MaterialCheckbox';
 
+import QuestionMarkCircle from '../../Images/Question_Mark_Circle.png';
+
 function PhishingPage() {
 
     return (
         <div className="phishing-page">
             <div className="opacity-overlay">
                 <div className="card">
-                    <div className="card-header"/>
+                    <div className="card-header" />
                     <div className="card-body">
                         <h5 className="card-heading">
                             Enter your Username and Password
                         </h5>
+                        <section className="form-section">
+                            <label>Username:</label>
+                            <input className="form-input" />
+                        </section>
+                        <section className="form-section">
+                            <label>Password:</label>
+                            <input className="form-input" />
+                        </section>
+                        <input type="button" className="submit-button" value="LOGIN"></input>
+                        <div className="forgot-password-container">
+                            <img src={QuestionMarkCircle} className="question-circle">
+
+                            </img>
+                            <span>Forgot your password?</span>
+                        </div>
+                        <p>
+                            For security reasons, please log out and exit your web browser when you are done accessing services that require authentication!                        </p>
                     </div>
                 </div>
             </div>
