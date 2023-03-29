761fe839eb2e1e983ba772a91aa1bef8901972ad
Garnet Yeates <garnetyeates8@gmail.com>
2023-03-29 15:14:56 -0400

    Backup Commit (changed reporting page)

--- src/Pages/ReportingPage/ReportingPage.jsx
+++ src/Pages/ReportingPage/ReportingPage.jsx
@@ -8,33 +8,87 @@ function ReportingPage({ scrollToTop }) {
     return (
         <div className="reporting-page-container">
             <div className="reporting-page">
-                
+                <Report />
             </div>
         </div>
     )
 }
 
+// Contains 2 tables
 function Report() {
+
     const [reportData, setReportData] = useState({});
 
+    const { allSubmissions = [], surveyAverages = {} } = reportData;
+
     useEffect(() => {
-        setReportData(getReportData());
+        const fetchData = async () => {
+            setReportData(await getReportData());
+        }
+
+        fetchData();
     }, [])
 
-
+    return (
+        <div className="tables-container">
+            <ParticipantReportTable allSubmissions={allSubmissions} />
+            <SurveyReportTable />
+        </div>
+    )
 }
 
 // For each participant, display score for each survey, as well as average 
 // (col headers are: [ParticipantName, Facebook, Amazon, ..., Avg])
-function ParticipantReportTable(allSubmissions) {
+function ParticipantReportTable({ allSubmissions }) {
 
+    console.log("all sub", allSubmissions)
+    return (
+        <table className="reporting-table">
+            <tr>
+                <th>Participant Name</th>
+                <th>Facebook</th>
+                <th>Amazon</th>
+                <th>TikTok</th>
+                <th>LinkedIn</th>
+                <th>Snapchat</th>
+                <th>Twitter</th>
+                <th>Youtube</th>
+                <th>Pinterest</th>
+                <th>Average</th>
+            </tr>
+            {allSubmissions.map((submission) => {
 
+                // Destructure Assignment, default evertything to "N/A"
+                const {
+                    participantFullName,
+                    facebook = "N/A",
+                    amazon = "N/A",
+                    tikTok = "N/A",
+                    linkedIn = "N/A",
+                    snapchat = "N/A",
+                    twitter = "N/A",
+                    youtube = "N/A",
+                    pinterest = "N/A",
+                    average } = submission;
+
+                const arr = [participantFullName, facebook, amazon, tikTok, linkedIn, snapchat, twitter, youtube, pinterest, average]
+
+                return (
+                    <tr>
+                        {arr.map((element) => <td>{element}</td>)}
+                    </tr>
+                )
+            })}
+        </table>
+    )
 }
 
 // For each survey, display the average
 // (col headers are: [Survey, AvgScore])
-function SurveyReportTabpe(surveyAverages) {
-
+function SurveyReportTable(surveyAverages) {
+    return (
+        <></>
+    )
 }
 
 const getReportData = async () => {
