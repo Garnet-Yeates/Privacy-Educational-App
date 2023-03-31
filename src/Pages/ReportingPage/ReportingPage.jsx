6f192a1ba16db756a23787600f9956f48719d135
Garnet Yeates <garnetyeates8@gmail.com>
2023-03-31 10:37:23 -0400

    Finalizing Reporting Page

--- src/Pages/ReportingPage/ReportingPage.jsx
+++ src/Pages/ReportingPage/ReportingPage.jsx
@@ -49,13 +49,13 @@ function Report() {
 
     return (
         <>
-            <GlobalAverage surveyAverages={surveyAverages}/>
-            <div className="tables-container">
-                <ParticipantReportTable allSubmissions={allSubmissions} surveyAverages={surveyAverages} />
-            </div>
-            
+            <h1 className="reporting-page-heading">
+                Surveys Report
+            </h1>
+            <GlobalAverage surveyAverages={surveyAverages} />
+            <SurveyAverages surveyAverages={surveyAverages} />
+            <ParticipantReportTable allSubmissions={allSubmissions} surveyAverages={surveyAverages} />
         </>
-
     )
 }
 
@@ -68,13 +68,55 @@ function GlobalAverage({ surveyAverages }) {
 
     return (
         <div className="global-average-container">
-            <ColoredScoreCounter end={globalAverage}/>
+            <ColoredScoreCounter score={globalAverage} />
             <div className="global-average-subheading">
                 Global Survey Average
             </div>
         </div>
     )
+}
 
+function SurveyAverages({ surveyAverages }) {
+
+    return (
+        <div className="survey-averages-container">
+            <div className="container px-0">
+                <div className="row justify-content-center">
+                    <SurveyAverage surveyAverages={surveyAverages} surveyName="facebook" />
+                    <SurveyAverage surveyAverages={surveyAverages} surveyName="amazon" />
+                    <SurveyAverage surveyAverages={surveyAverages} surveyName="tikTok" />
+                    <SurveyAverage surveyAverages={surveyAverages} surveyName="linkedIn" />
+                    <SurveyAverage surveyAverages={surveyAverages} surveyName="snapchat" />
+                    <SurveyAverage surveyAverages={surveyAverages} surveyName="twitter" />
+                    <SurveyAverage surveyAverages={surveyAverages} surveyName="youtube" />
+                    <SurveyAverage surveyAverages={surveyAverages} surveyName="pinterest" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function SurveyAverage({ surveyAverages, surveyName }) {
+
+    const score = surveyAverages[surveyName];
+
+    if (score === undefined)
+        return <></>
+
+    return (
+        <div className="col-6 col-md-4 col-lg-3">
+            <div className="individual-average-container">
+                <ColoredScoreCounter score={score} />
+                <div className="individual-average-subheading">
+                    {capitalizeFirst(surveyName)} Average
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function capitalizeFirst(string) {
+    return string.substring(0, 1).toUpperCase() + string.substring(1, string.length);
 }
 
 // For each participant, display score for each survey, as well as average 
@@ -88,38 +130,40 @@ function ParticipantReportTable({ allSubmissions, surveyAverages }) {
     surveyAveragesRow = surveyAveragesRow.map((element) => typeof element === 'number' && +(element).toFixed(2) || element);
 
     return (
-        <table className="reporting-table">
-            <thead>
-                <tr>
-                    <th>Participant Name</th>
-                    <th>Facebook</th>
-                    <th>Amazon</th>
-                    <th>TikTok</th>
-                    <th>LinkedIn</th>
-                    <th>Snapchat</th>
-                    <th>Twitter</th>
-                    <th>Youtube</th>
-                    <th>Pinterest</th>
-                    <th>Average</th>
-                </tr>
-            </thead>
-            <tbody>
-                {<tr>{surveyAveragesRow.map((element, index) => <td key={index}>{element}</td>)}</tr>}
-                {allSubmissions.map((submission, index) => {
+        <div className="container participant-report-table-container px-0">
+            <table className="reporting-table">
+                <thead>
+                    <tr>
+                        <th>Participant Name</th>
+                        <th>Facebook</th>
+                        <th>Amazon</th>
+                        <th>TikTok</th>
+                        <th>LinkedIn</th>
+                        <th>Snapchat</th>
+                        <th>Twitter</th>
+                        <th>Youtube</th>
+                        <th>Pinterest</th>
+                        <th>Average</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {<tr>{surveyAveragesRow.map((element, index) => <td key={index}>{element}</td>)}</tr>}
+                    {allSubmissions.map((submission, index) => {
 
-                    // Destructure Assignment, default evertything to "N/A"
-                    const { participantFullName, facebook = "N/A", amazon = "N/A", tikTok = "N/A", linkedIn = "N/A", snapchat = "N/A", twitter = "N/A", youtube = "N/A", pinterest = "N/A", average } = submission;
+                        // Destructure Assignment, default evertything to "N/A"
+                        const { participantFullName, facebook = "N/A", amazon = "N/A", tikTok = "N/A", linkedIn = "N/A", snapchat = "N/A", twitter = "N/A", youtube = "N/A", pinterest = "N/A", average } = submission;
 
-                    const row = [participantFullName, facebook, amazon, tikTok, linkedIn, snapchat, twitter, youtube, pinterest, fixNumber(average)]
+                        const row = [participantFullName, facebook, amazon, tikTok, linkedIn, snapchat, twitter, youtube, pinterest, fixNumber(average)]
 
-                    return (
-                        <tr key={index}>
-                            {row.map((element, index) => <td key={index}>{element}</td>)}
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+                        return (
+                            <tr key={index}>
+                                {row.map((element, index) => <td key={index}>{element}</td>)}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
@@ -137,9 +181,9 @@ const getReportData = async () => {
     }
 }
 
-function ColoredScoreCounter({ end }) {
+function ColoredScoreCounter({ score }) {
 
-    const { value } = useCountUp({ isCounting: true, end, duration: 5, decimalPlaces: 0 })
+    const { value } = useCountUp({ isCounting: true, end: score, duration: 5, decimalPlaces: 0 })
 
     return (
         <div className="colored-score-counter" style={{ "--underline-color": `hsla(${Math.floor(value * 1.15)}, 90%, 50%, 0.5)` }}>
